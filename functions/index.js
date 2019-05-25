829abd197b4f9f7b28cea679985006e5618a62ad
CONWAY <kable2580@gmail.com>
2019-05-25 21:52:57 +0800

    pairing system prototype complete

--- functions/index.js
+++ functions/index.js
@@ -5,48 +5,77 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.requestpair = functions.https.onRequest((request, response) => {
+exports.requestpair = functions.https.onRequest(async (request, response) => {
     if (request.method === 'POST') {
         const pairRequestUid = request.body;
         console.log("pair uid: " + pairRequestUid);
-        const db = admin.database().ref('/pairing');
 
-        db.once('value').then(async (snapshot) => {
-            let waitingUsers = [];
+        const pairingDb = admin.database().ref('/pairing');
+        const usersDb = admin.database().ref('/users');
+        const roomsDb = admin.database().ref('/rooms');
+
+        // get waiting users
+        let waitingUsers = [];
+        await pairingDb.once('value').then((snapshot) => {
             snapshot.forEach((child) => {
                 waitingUsers.push({pushId: child.key, uid: child.val()['uid']});
             });
-
-            console.log(waitingUsers);
-
-            // if more than two people are waiting to pair
-            if (waitingUsers.length > 0) {
-                const uid1 = pairRequestUid;
-                const uid2 = waitingUsers[0].uid;
-                console.log('pairing ' + uid1 + ' and ' + uid2);
-                const roomId = uid1 + "_" + uid2;
-
-                await admin.database().ref('/users').once('value').then((snapshot) => {
-                    snapshot.forEach(async (child) => {
-                        if (child.val()['uid'] === uid1 || child.val()['uid'] === uid2) {
-                            await admin.database().ref('/rooms/' + roomId).child('tags').set(['fuck', 'you']);
-
-
-                            // send notification
-                            const token = child.val()['token'];
-                            const payload = "{notification:{title:" + roomId + ",body: \"配對成功\"}}";
-                            console.log('send notification');
-                            await admin.messaging().sendToDevice(token, payload);
-                        }
-                    })
-                });
-                console.log('paired ' + pairRequestUid + ' ' + waitingUsers[0].uid);
-                console.log('remove pushId ' + waitingUsers[0].pushId);
-                await db.child(waitingUsers[0].pushId).set(null);
-            } else {
-                await db.push({uid: pairRequestUid});
-            }
         });
+
+        console.log(waitingUsers);
+
+        // if more than two people are waiting to pair
+        if (waitingUsers.length > 0) {
+            // remove uid from waiting queue
+            await pairingDb.child(waitingUsers[0].pushId).set(null);
+
+            const uid1 = pairRequestUid;
+            const uid2 = waitingUsers[0].uid;
+            console.log('pairing ' + uid1 + ' and ' + uid2);
+            const roomId = uid1 + "_" + uid2;
+
+            // get tags and tokens
+            let tag1 = '';
+            let tag2 = '';
+            let token1 = '';
+            let token2 = '';
+
+            await usersDb.once('value').then((snapshot) => {
+                snapshot.forEach((child) => {
+                    if (child.val()['uid'] === uid1) {
+                        tag1 = child.val()['tags'][0];
+                        token1 = child.val()['token'];
+                    } else if (child.val()['uid'] === uid2) {
+                        tag2 = child.val()['tags'][0];
+                        token2 = child.val()['token'];
+                    }
+                })
+            });
+
+            console.log(tag1, token1, tag2, token2);
+
+            // create room
+            await roomsDb.child(roomId + '/tags').set([tag1, tag2]);
+
+            // send notification
+            // const payload = "{notification:{title:" + roomId + ",body: \"配對成功\"}}";
+            const payload = {
+                notification: {
+                    title: roomId,
+                    body: '配對成功'
+                }
+            };
+
+            console.log('send notification to ' + token1);
+            await admin.messaging().sendToDevice(token1, payload);
+            console.log('send notification to ' + token2);
+            await admin.messaging().sendToDevice(token2, payload);
+
+        } else {
+            // else push uid into waiting queue
+            await pairingDb.push({uid: pairRequestUid});
+        }
+
         response.statusCode = 200;
         response.send(pairRequestUid);
         response.end();
@@ -56,16 +85,16 @@ exports.requestpair = functions.https.onRequest((request, response) => {
     }
 });
 
-exports.cancelpair = functions.https.onRequest((request, response) => {
+exports.cancelpair = functions.https.onRequest(async (request, response) => {
     if (request.method === 'POST') {
         const cancelpairuid = request.body;
         console.log("cancel pair uid: " + cancelpairuid);
         const db = admin.database().ref('/pairing');
 
-        db.once('value').then((snapshot) => {
-            snapshot.forEach(async (child) => {
+        await db.once('value').then((snapshot) => {
+            snapshot.forEach((child) => {
                 if (child.val()['uid'] === cancelpairuid)
-                    await db.child(child.key).set(null);
+                    db.child(child.key).set(null);
             });
         });
 
@@ -76,19 +105,4 @@ exports.cancelpair = functions.https.onRequest((request, response) => {
         response.statusCode = 405;
         response.send('HTTP Method ' + request.method + ' not allowed');
     }
-});
-
-exports.test = functions.https.onRequest(async (req, resp) => {
-    const token = "dMMyQabd5bw:APA91bG9OVMOjOFKfOgXXS3jhUvHD0nz7hHFJ6a-40EtOxXxqJuLH7JDzy2yoA767IsJUxQnpxIurPilS1p4njVg1vM4y2Fg3TuDXJxBGNK1iaQN4VNfkhIGVX3NmBIgFf1rzPaM7f6F";
-    const payload = "{notification: {title: \"title1\", body: \"body1\"}}";
-
-    const roomId = 'werqwer';
-
-    await admin.database().ref('/rooms/' + roomId).child('tags').set(['fuck', 'you']);
-
-
-    console.log('send notification');
-    await admin.messaging().sendToDevice(token, [payload]);
-    resp.statusCode = 200;
-    resp.end();
 });
